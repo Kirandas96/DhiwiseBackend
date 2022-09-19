3ae305d073bfae5f613cd91c44cf4df04f75c75d
Kirandas96 <parvathikiran10@gmail.com>
2022-09-19 10:21:49 +0530

    done

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express=require("express")
 const connection = require("./db")
 const TableRouter=require("./Route/TableRoute")
 require("dotenv").config();
+const cors=require("cors")
 
 const app=express()
 const port=process.env.PORT || 5000 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-
+app.use(cors({
+    origin:["http://localhost:3000/"]
+}))
 
 
 app.get("/",(req,res)=>{
